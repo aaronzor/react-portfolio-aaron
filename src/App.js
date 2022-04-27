b3198046da4a742b4573750764e13511e1cae68d
aaronzor <mcmaaron@gmail.com>
2022-04-27 22:04:30 +0100

    Create files for 'About me' section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// Import Styles
 import './App.css';
+
+// Component Imports
 import Home from './components/home/Home';
+import AboutMe from './components/aboutMe/AboutMe';
 
 function App() {
   return (
     <div className='App'>
       <Home />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+// Main Imports
+import React from 'react';
+
+// Import Styles
+import './aboutMe.css';
+
+const AboutMe = () => {
+  return <div>AboutMe</div>;
+};
+
+export default AboutMe;
