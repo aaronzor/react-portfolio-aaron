bc2eb8fcd25d18a305ae83389413b9598dffc633
aaronzor <mcmaaron@gmail.com>
2022-04-30 19:31:18 +0100

    Add container and CSS to Layout - for centering content

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -8,13 +8,15 @@ import './layout.css';
 const Layout = (props) => {
   return (
     <section className={props.title.toLowerCase()}>
-      <Fade left duration={1000} distance='70px'>
-        <h1 className='section-title'>{props.title}</h1>
-      </Fade>
-      <Fade right duration={1000}>
-        <div className='underline'></div>
-      </Fade>
-      {props.children}
+      <div className='section-main'>
+        <Fade left duration={1000} distance='70px'>
+          <h1 className='section-title'>{props.title}</h1>
+        </Fade>
+        <Fade right duration={1000}>
+          <div className='underline'></div>
+        </Fade>
+        {props.children}
+      </div>
     </section>
   );
 };
